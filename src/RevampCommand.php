8309353298b6da6fb651508a05c60430f6f0ff22
Rias <hey@rias.be>
2026-05-06 19:45:12 +0200

    Update version constraints

--- src/RevampCommand.php
+++ src/RevampCommand.php
@@ -246,8 +246,8 @@ class RevampCommand extends Command
     {
         $config = Json::decodeFromFile($composerJsonPath);
 
-        $config['require']['craftcms/cms'] = '6.x-dev as 5.9.0';
-        $config['require']['craftcms/yii2-adapter'] = '6.x-dev as 5.9.0';
+        $config['require']['craftcms/cms'] = '^6.0.0-alpha.1 as 5.9.0';
+        $config['require']['craftcms/yii2-adapter'] = '^6.0.0-alpha.1 as 5.9.0';
 
         if (isset($config['require-dev']['craftcms/generator'])) {
             $config['require-dev']['craftcms/generator'] = '3.x-dev';
